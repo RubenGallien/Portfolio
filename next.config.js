6190d626e4e9e0110333379d07490d7ddeb528a6
RubenGallien <ruben-gallienpro@outlook.fr>
2023-01-17 14:11:05 +0100

    retest

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'https://rubengallien.vercel.app/'
+    ]
+  }
 }
 
 module.exports = nextConfig
